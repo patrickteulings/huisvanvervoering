538ca7f7c209eede2052ba1962b5a96ace4c012c
Patrick Teulings <patrickteulings@gmail.com>
2021-04-16 10:43:08 +0200

    Quote and Cite
    Fixed Quote width on homepage. Cite is now better positioned responsively

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -69,33 +69,33 @@
 
 
   <div class="entry-content">
-    <?php function page_summary()
-    {
-      global $post;
-      $args = array(
-        'post_type'      => 'page',
-        'posts_per_page' => -1,
-        'post_parent'    => $post->ID,
-        'order'          => 'ASC',
-        'orderby'        => 'menu_order'
-      );
-
-
-      $parent = new WP_Query($args);
-
-      if ($parent->have_posts()) :
-
-        while ($parent->have_posts()) : $parent->the_post(); ?>
-          <div id="parent-<?php the_ID(); ?>" class="parent-page">
-
-            <h1><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
-          </div>
     <?php
-        endwhile;
 
-      endif;
-      wp_reset_postdata();
-    } ?>
+    global $post;
+    $args = array(
+      'post_type'      => 'page',
+      'posts_per_page' => -1,
+      'post_parent'    => $post->ID,
+      'order'          => 'ASC',
+      'orderby'        => 'menu_order'
+    );
+
+
+    $parent = new WP_Query($args);
+
+    if ($parent->have_posts()) :
+
+      while ($parent->have_posts()) : $parent->the_post(); ?>
+        <div id="parent-<?php the_ID(); ?>" class="parent-page">
+
+          <h1><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+        </div>
+    <?php
+      endwhile;
+
+    endif;
+    wp_reset_postdata();
+    ?>
     <?php
     the_content();
 
